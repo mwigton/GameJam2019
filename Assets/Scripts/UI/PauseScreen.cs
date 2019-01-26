7e6a7be70f8da29f0104a84f844e8c2bd62e58ae
Michael Wigton <mwigton@epsilonsystems.com>
2019-01-25 19:50:14 -0700

    start pause screen

---
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace GameJam
+{
+	public class PauseScreen : MonoBehaviour
+	{
+		public GameObject pauseScreen;
+		private bool m_paused = false;
+
+		public void Update()
+		{
+			if(Input.GetKeyDown(KeyCode.Escape))
+			{
+				m_paused = !m_paused;
+				SetPaused(m_paused);
+			}
+		}
+
+		public void SetPaused(bool paused)
+		{
+			Debug.Log(paused);
+			pauseScreen.SetActive(m_paused);
+			Time.timeScale = m_paused ? 0 : 1;
+			Cursor.lockState = m_paused ? CursorLockMode.None : CursorLockMode.Confined;
+		}
+	}
+}
