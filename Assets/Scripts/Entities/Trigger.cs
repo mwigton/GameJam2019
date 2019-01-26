e6928647a9e3c3a4c4b234c92de632578cb52e13
Michael Wigton <mwigton@epsilonsystems.com>
2019-01-25 21:37:22 -0700

    remove github plugin

---
+++ Assets/Scripts/Entities/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameJam
+{
+	public class Trigger : MonoBehaviour
+	{
+		public UnityEvent Entered = new UnityEvent();
+		public UnityEvent Exited = new UnityEvent();
+
+		private void OnTriggerEnter(Collider other)
+		{
+			Entered.Invoke();			
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			Exited.Invoke();	
+		}
+	}
+}
