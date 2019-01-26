3be6749da1ac6d5e0c0154e2258972a742593cd6
Michael Wigton <mwigton@epsilonsystems.com>
2019-01-26 16:43:31 -0700

    update scene

---
+++ Assets/Scripts/UI/CanvasGroupFader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace GameJam
+{
+	public class CanvasGroupFader : MonoBehaviour
+	{
+		[SerializeField]
+		private CanvasGroup m_canvasGroup;
+
+		[SerializeField]
+		private float m_duration;
+
+		public void Show()
+		{
+			m_canvasGroup.DOFade(1, m_duration);
+			m_canvasGroup.interactable = true;
+			m_canvasGroup.blocksRaycasts = true;
+		}
+
+		public void Hide()
+		{
+			m_canvasGroup.DOFade(0, m_duration);
+			m_canvasGroup.interactable = false;
+			m_canvasGroup.blocksRaycasts = false;
+		}
+	}
+}
