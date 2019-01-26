02e1cede7c5850aa541da35b9138c3e4274c1c04
Michael Wigton <mwigton@epsilonsystems.com>
2019-01-26 10:14:02 -0700

    add basic door

---
+++ Assets/Scripts/Entities/Door.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GameJam
+{
+	public class Door : MonoBehaviour
+	{
+		public string item;
+		public GameObject door;
+
+		public void CheckOpen()
+		{
+			if(Inventory.HasItem(item))
+			{
+				door.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameJam
+{
+	public class InventoryItem : MonoBehaviour
+	{
+		public GameObject model;
+		public string itemName;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.tag != "Player") return;
+			Inventory.Add(itemName);
+			gameObject.SetActive(false);
+		}
+	}
+}
