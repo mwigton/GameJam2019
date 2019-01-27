1b4d9cbc585cb8054c4cb3ba0b5db1d2f6a8b0e2
Michael Wigton <mwigton@epsilonsystems.com>
2019-01-26 18:50:12 -0700

    update player items

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -6,16 +6,21 @@ namespace GameJam
 	{
 		public GameObject model;
 		public string itemName;
+		public AudioSource audioSource;
+		public GameObject playerModelItem;
 
 		public bool Collected { get; private set; }
 
 		private void OnTriggerEnter(Collider other)
 		{
+			if (Collected) return;
 			if (other.tag != "Player") return;
 			Debug.Log("Item collected: " + gameObject.name, this);
+			audioSource.Play();
 			Collected = true;
 			Inventory.Add(itemName);
-			gameObject.SetActive(false);
+			model.SetActive(false);
+			playerModelItem.SetActive(true);
 		}
 	}
 }
---
+++ Assets/Scripts/Util/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GameJam
+{
+	public class Rotate : MonoBehaviour
+	{
+		public Vector3 speed;
+
+		private void Update()
+		{
+			transform.Rotate(speed * Time.deltaTime);
+		}
+	}
+}
